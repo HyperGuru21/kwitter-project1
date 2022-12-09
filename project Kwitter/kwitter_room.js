e32099533079bd121262d42d6a12c3bf6554f18f
HyperGuru21 <94612217+HyperGuru21@users.noreply.github.com>
2022-12-09 18:56:57 +0530

    Add files via upload

--- project Kwitter/kwitter_room.js
+++ project Kwitter/kwitter_room.js
@@ -1,8 +1,29 @@
-
+const firebaseConfig = {
+      apiKey: "AIzaSyBs8S2-33X7pgv1jUaTTeiOckVVIhKQCr0",
+      authDomain: "project-kwitter-13c65.firebaseapp.com",
+      databaseURL:"https://project-kwitter-13c65-default-rtdb.firebaseio.com",
+      projectId: "project-kwitter-13c65",
+      storageBucket: "project-kwitter-13c65.appspot.com",
+      messagingSenderId: "1090341654855",
+      appId: "1:1090341654855:web:c062df8a4979650eb43c62"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 //ADD YOUR FIREBASE LINKS HERE
 
+function logout(){
+
+      window.location="index.html"
+
+};
+
+
+
+
+
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
+       room_name = childKey;
       //Start code
 
       //End code
